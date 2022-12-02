6d3322e3991a0506e90b048086ee693cd977ea41
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2022-12-02 18:47:32 +0530

    T

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,21 +3,9 @@
   import Footer from "./Footer.svelte";
   import Navbar from "./Navbar.svelte";
   import { inject } from "@vercel/analytics";
-  import  {webVitals}  from "$lib/vitals";
-  import { browser } from "$app/environment";
-  import { page } from "$app/stores";
-
-  let analyticsId = import.meta.env.VERCEL_ANALYTICS_ID;
-  $: if (browser && analyticsId) {
-    webVitals({
-      path: $page.url.pathname,
-      params: $page.params,
-      analyticsId,
-    });
-  }
-
   inject();
 </script>
+
 
 <Navbar />
 <slot />
