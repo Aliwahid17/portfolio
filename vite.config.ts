ed8c8ba62e0fed3636dc309eee9c4a44a1a66f0a
wahid ali <88263575+Aliwahid17@users.noreply.github.com>
2023-02-10 00:42:14 +0530

    test 1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import {join} from 'path';
+import { partytownVite } from '@builder.io/partytown/utils';
 
 const config: UserConfig = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), partytownVite({
+		dest: join(process.cwd(), 'script', '~partytown')
+	})],
 	define: {
 		'import.meta.env.VERCEL_ANALYTICS_ID': JSON.stringify(process.env.VERCEL_ANALYTICS_ID)
 	},
